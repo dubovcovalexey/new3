30f1f880e66478c696e3f335154b180f265227f3
dubovcovalexey <134150801+dubovcovalexey@users.noreply.github.com>
2023-06-09 18:57:27 +0300

    app.py

--- app.py
+++ app.py
@@ -30,29 +30,22 @@ def set_png_as_page_bg(png_file):
 set_png_as_page_bg('6.JPG')
 
 
-classifier_name=['XGBoost', 'Гадание на кофейной гуще']
-option = st.sidebar.selectbox('Евгений Викторович, какой алгоритм запустить?', classifier_name)
+classifier_name=['XGBoost']
+option = st.sidebar.selectbox('Модель прогнозирования оттока клиентов', classifier_name)
 st.subheader(option)
 
 
 
 #Importing model and label encoders
-model=pickle.load(open("final_xg_model.pkl","rb"))
+model=pickle.load(open("model_saved","rb"))
 #model_1 = pickle.load(open("final_rf_model.pkl","rb"))
-le_pik=pickle.load(open("label_encoding_for_gender.pkl","rb"))
-le1_pik=pickle.load(open("label_encoding_for_geo.pkl","rb"))
 
 
-def predict_churn(CreditScore, Geo, Gen, Age, Tenure, Balance, NumOfProducts, HasCrCard, IsActiveMember, EstimatedSalary):
-    input = np.array([[CreditScore, Geo, Gen, Age, Tenure, Balance, NumOfProducts, HasCrCard, IsActiveMember, EstimatedSalary]]).astype(np.float64)
-    if option == 'XGBoost':
-        prediction = model.predict_proba(input)
-        pred = '{0:.{1}f}'.format(prediction[0][0], 2)
 
-    else:
-        pred=0.30
-        #st.markdown('Наверное, клиент останется в банке, но это не точно да и вообще надо звонить в Битву экстрасенсов.')
-
+def predict_churn(CreditScore, Geography, Gender, Age, Tenure, Balance, NumOfProducts, HasCrCard, IsActiveMember, EstimatedSalary):
+    input = np.array([[CreditScore, Geography, Gender, Age, Tenure, Balance, NumOfProducts, HasCrCard, IsActiveMember, EstimatedSalary]])
+    prediction = model.predict_proba(input)
+    pred = '{0:.{1}f}'.format(prediction[0][0], 2)
     return float(pred)
 
 
@@ -69,31 +62,21 @@ def main():
 
 
 
-    st.sidebar.subheader("Приложение создано для курса Diving into Darkness of Data Science")
-    st.sidebar.text("Разработчик - Братковский Е.В.")
+    st.sidebar.subheader("Итоговая работа в рамках курса Diving into Darkness of Data Science")
+    st.sidebar.text("Разработчик - Дубовцов А.А.")
 
-
-    CreditScore = st.slider('Скоринговый балл', 300, 900)
-
-    Geography = st.selectbox('География/регион', ['France', 'Germany', 'Spain'])
-    Geo = int(le1_pik.transform([Geography]))
-
-    Gender = st.selectbox('Пол', ['Male', 'Female'])
-    Gen = int(le_pik.transform([Gender]))
-
-    Age = st.slider("Возраст", 10, 95)
-
-    Tenure = st.selectbox("Стаж", ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9','10', '11', '12', '13', '14', '15'])
-
-    Balance = st.slider("Баланс", 0.00, 250000.00)
-
-    NumOfProducts = st.selectbox('Количество продуктов', ['1', '2', '3', '4'])
-
+    CreditScore = st.slider('Скоринговый балл', 0, 400)
+    Geography = st.selectbox('География/регион', ['Минск', 'Брест', 'Могилев'])
+    gen =  Gender.replace(1, "Женский").replace(2, "Мужской")
+    Gen = st.selectbox('Пол', ['Женский', 'Мужской'])
+    Age = st.slider("Возраст", 10, 100)
+    Tenure = st.selectbox("Стаж",
+                          ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13', '14', '15'])
+    Balance = st.slider("Баланс", 0.00, 10000.00)
+    NumOfProducts = st.selectbox('Количество продуктов', ['1', '2'])
     HasCrCard = st.selectbox("Есть кредитная БПК ?", ['0', '1'])
-
     IsActiveMember = st.selectbox("Активный клиент ?", ['0', '1'])
-
-    EstimatedSalary = st.slider("Зарплата", 0.00, 200000.00)
+    EstimatedSalary = st.slider("Зарплата", 0.00, 10000.00)
 
     churn_html = """  
               <div style="background-color:#f44336;padding:20px >
@@ -119,3 +102,4 @@ def main():
 
 if __name__=='__main__':
     main()
+
