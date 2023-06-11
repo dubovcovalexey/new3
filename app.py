10d31d395832fe6349e858c2012e9f4a3b98911d
dubovcovalexey <134150801+dubovcovalexey@users.noreply.github.com>
2023-06-11 22:26:00 +0300

    app.py

--- app.py
+++ app.py
@@ -122,17 +122,16 @@ def main():
         st.markdown(churn_html, unsafe_allow_html= True)
 
     
-    else:
-        st.button('Сделать прогноз'):
-            output = predict_churn(CreditScore, Geography, Gender, Age, Tenure, Balance, NumOfProducts, HasCrCard, IsActiveMember, EstimatedSalary)
-            st.success('Вероятность оттока составляет {:.2f} %'.format(output))
-            if output >= 85:
-                st.markdown(churn_html, unsafe_allow_html= True)
-            elif output >= 40:
-                st.markdown(mb_churn_html, unsafe_allow_html= True)
-            else:
-                st.markdown(no_churn_html, unsafe_allow_html= True)
-                st.balloons()
+    elif st.button('Сделать прогноз'):
+        output = predict_churn(CreditScore, Geography, Gender, Age, Tenure, Balance, NumOfProducts, HasCrCard, IsActiveMember, EstimatedSalary)
+        st.success('Вероятность оттока составляет {:.2f} %'.format(output))
+        if output >= 85:
+            st.markdown(churn_html, unsafe_allow_html= True)
+        elif output >= 40:
+            st.markdown(mb_churn_html, unsafe_allow_html= True)
+        else:
+            st.markdown(no_churn_html, unsafe_allow_html= True)
+            st.balloons()
 
 
 if __name__=='__main__':
