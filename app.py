0af2ced3a09586f52a0a917a2bfa69214e84fb91
dubovcovalexey <134150801+dubovcovalexey@users.noreply.github.com>
2023-06-11 14:49:17 +0300

    app.py

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ def set_png_as_page_bg(png_file):
 
     st.markdown(page_bg_img, unsafe_allow_html=True)
     return
-set_png_as_page_bg('6.JPG')
+set_png_as_page_bg('6.jpg')
 
 
 classifier_name=['Сatboost']
