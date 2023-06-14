cff6217723a5c2518d94a0ecacac41d0fde5ce70
dubovcovalexey <134150801+dubovcovalexey@users.noreply.github.com>
2023-06-14 23:26:46 +0300

    app.py

--- app.py
+++ app.py
@@ -122,9 +122,9 @@ def main():
     
         if Age - Tenure < 17:
             st.error('Некорректный возраст клиента или длительность обслуживания в банке')
-            break
+            
         
-        if Balance < 1000 and EstimatedSalary < 500 and IsActiveMember == 0 and NumOfProducts == 1:
+        elif Balance < 1000 and EstimatedSalary < 500 and IsActiveMember == 0 and NumOfProducts == 1:
             st.success('Вероятность оттока составляет более 90%.')
             st.markdown(churn_html, unsafe_allow_html= True)
 
