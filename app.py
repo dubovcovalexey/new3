b3f2da98a259ea4418326295faf46996c514981a
dubovcovalexey <134150801+dubovcovalexey@users.noreply.github.com>
2023-06-14 23:50:49 +0300

    app.py

--- app.py
+++ app.py
@@ -128,7 +128,10 @@ def main():
             st.success('Вероятность оттока составляет более 90%.')
             st.markdown(churn_html, unsafe_allow_html= True)
 
-
+        elif Balance > 2000 and EstimatedSalary > 2000 and CreditScore > 250:
+            st.success('Вероятность оттока составляет менее 40 %.')
+            st.markdown(no_churn_html, unsafe_allow_html= True)
+                
         else:
             output = predict_churn(CreditScore, Geography, Gender, Age, Tenure, Balance, NumOfProducts, HasCrCard, IsActiveMember, EstimatedSalary)
             st.success('Вероятность оттока составляет {:.2f} %'.format(output))
